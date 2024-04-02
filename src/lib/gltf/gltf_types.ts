200ae4314fe91c389ddb828030371bcbfef2059b
Joe Grinstead <npm@mailforce.net>
2024-04-02 15:07:09 -0400

    Add more gltf types

--- src/lib/gltf/gltf_types.ts
+++ src/lib/gltf/gltf_types.ts
@@ -2,6 +2,34 @@ export type GLTFTarget = number;
 export const GLTF_ARRAY_BUFFER = 34962;
 export const GLTF_ELEMENT_ARRAY_BUFFER = 34963;
 
+export const GLTF_ACCESSOR_INT8 = 5120;
+export const GLTF_ACCESSOR_UINT8 = 5121;
+export const GLTF_ACCESSOR_INT16 = 5122;
+export const GLTF_ACCESSOR_UINT16 = 5123;
+export const GLTF_ACCESSOR_UINT32 = 5125;
+/** values of NaN, +Infinity, -Infinity MUST NOT be present. */
+export const GLTF_ACCESSOR_F32 = 5126;
+
+export type GltfAccessorType =
+  | typeof GLTF_ACCESSOR_INT8
+  | typeof GLTF_ACCESSOR_UINT8
+  | typeof GLTF_ACCESSOR_INT16
+  | typeof GLTF_ACCESSOR_UINT16
+  | typeof GLTF_ACCESSOR_UINT32
+  | typeof GLTF_ACCESSOR_F32;
+
+export const GLTF_ACCESSOR_LENGTH = {
+  SCALAR: 1,
+  VEC2: 2,
+  VEC3: 3,
+  VEC4: 4,
+  MAT2: 4,
+  MAT3: 9,
+  MAT4: 16,
+};
+
+export type GltfAccessorElementType = keyof typeof GLTF_ACCESSOR_LENGTH;
+
 export type GLTFVersion = string;
 
 export type GLTFAssetMetaData = {
@@ -184,6 +212,124 @@ export type GLTFBufferView = {
   extras?: unknown;
 };
 
+/**
+ * (TODO) unfinished
+ *
+ * Sparse encoding of arrays is often more memory-efficient than dense encoding
+ * when describing incremental changes with respect to a reference array. This
+ * is often the case when encoding morph targets (it is, in general, more
+ * efficient to describe a few displaced vertices in a morph target than
+ * transmitting all morph target vertices).
+ *
+ * Similar to a standard accessor, a sparse accessor initializes an array of
+ * typed elements from data stored in a bufferView. When accessor.bufferView is
+ * undefined, the sparse accessor is initialized as an array of zeros of size
+ * (size of the accessor element) * (accessor.count) bytes.
+ *
+ * On top of that, a sparse accessor includes a sparse JSON object describing
+ * the elements that are different from their initialization values.
+ */
+export type GLTFSparseAccessor = {
+  /**
+   * number of displaced elements. This number MUST NOT be greater than the
+   * number of the base accessor elements.
+   */
+  count: number;
+
+  // indices:
+};
+
+/**
+ * All binary data for meshes, skins, and animations is stored in buffers and
+ * retrieved via accessors.
+ *
+ * An accessor defines a method for retrieving data as typed arrays from within
+ * a buffer view. The accessor specifies a component type (e.g., float) and a
+ * data type (e.g., VEC3 for 3D vectors), which when combined define the
+ * complete data type for each data element. The number of elements is specified
+ * using the count property. Elements could be, e.g., vertex indices, vertex
+ * attributes, animation keyframes, etc.
+ *
+ * All accessors are stored in the asset’s accessors array.
+ */
+export type GLTFAccessor = {
+  bufferView?: GLTFIndex<"bufferViews">;
+
+  /**
+   * All binary data for meshes, skins, and animations is stored in buffers and
+   * retrieved via accessors.
+   *
+   * An accessor defines a method for retrieving data as typed arrays from
+   * within a buffer view. The accessor specifies a component type (e.g., float)
+   * and a data type (e.g., VEC3 for 3D vectors), which when combined define the
+   * complete data type for each data element. The number of elements is
+   * specified using the count property. Elements could be, e.g., vertex
+   * indices, vertex attributes, animation keyframes, etc.
+   */
+  byteOffset?: number;
+
+  /**
+   * The datatype of the accessor's components.  UNSIGNED_INT type **MUST NOT**
+   * be used for any accessor that is not referenced by
+   * `mesh.primitive.indices`.
+   *
+   * `type` parameter of `vertexAttribPointer()`.  The corresponding typed
+   * arrays are `Int8Array`, `Uint8Array`, `Int16Array`, `Uint16Array`,
+   * `Uint32Array`, and `Float32Array`.
+   */
+  componentType: GltfAccessorType;
+
+  /**
+   * Specifies whether integer data values are normalized (`true`) to [0, 1]
+   * (for unsigned types) or to [-1, 1] (for signed types) when they are
+   * accessed. This property **MUST NOT** be set to `true` for accessors with
+   * `FLOAT` or `UNSIGNED_INT` component type.
+   *
+   * `normalized` parameter of `vertexAttribPointer()`
+   */
+  normalized?: boolean;
+
+  /**
+   * The number of elements referenced by this accessor, not to be confused with
+   * the number of bytes or number of components.
+   *
+   * MUST be a positive integer
+   */
+  count: number;
+
+  /**
+   *
+   */
+  type: GltfAccessorElementType;
+
+  /**
+   * accessor.min and accessor.max properties are arrays that contain
+   * per-component minimum and maximum values, respectively. The length of these
+   * arrays MUST be equal to the number of accessor’s components.
+   *
+   * Values stored in glTF JSON MUST match actual minimum and maximum binary
+   * values stored in buffers. The accessor.normalized flag has no effect on
+   * these properties.
+   *
+   * A sparse accessor min and max properties correspond, respectively, to the
+   * minimum and maximum component values once the sparse substitution is
+   * applied.
+   *
+   * When neither sparse nor bufferView is defined, min and max properties MAY
+   * have any values. This is intended for use cases when binary data is
+   * supplied by external means (e.g., via extensions).
+   *
+   * For floating-point components, JSON-stored minimum and maximum values
+   * represent single precision floats and SHOULD be rounded to single precision
+   * before usage to avoid any potential boundary mismatches.
+   *
+   * Animation input and vertex position attribute accessors MUST have
+   * accessor.min and accessor.max defined. For all other accessors, these
+   * properties are optional.
+   */
+  max?: Array<number>;
+};
+
 export type GLTFScene = {
   name?: string;
 
@@ -242,6 +388,17 @@ export type GLTFTransRotScale = {
   rotation?: GLTFQuarternion;
 };
 
+// export type GLTFStandardAttributes;
+
+export type GLTFMeshPrimitive<Attributes extends string = string> = Record<
+  Attributes,
+  number
+>;
+
+export type GLTFMesh = {
+  primitives?: Array<GLTFMeshPrimitive>;
+};
+
 export type GLTFNode = GLTFTransRotScale & {
   name?: string;
   children?: Array<GLTFIndex<"nodes">>;
@@ -263,6 +420,13 @@ export type GLTFNode = GLTFTransRotScale & {
    * is identical to its local transformation matrix.
    */
   matrix?: Array<number>;
+
+  /**
+   * Any node MAY contain one mesh, defined in its mesh property. The mesh MAY
+   * be skinned using information provided in a referenced skin object. The mesh
+   * MAY have morph targets.
+   */
+  mesh?: GLTFIndex<"meshes">;
 };
 
 /**
@@ -274,8 +438,20 @@ export type GLTFAnimationTargetNode = GLTFNode & { matrix?: undefined };
 
 export type GLTFAsset = {
   asset: GLTFAssetMetaData;
+  accessors?: Array<GLTFAccessor>;
   buffers?: Array<GLTFBuffer>;
   bufferViews?: Array<GLTFBufferView>;
+
+  /**
+   * Meshes are defined as arrays of primitives. Primitives correspond to the
+   * data required for GPU draw calls. Primitives specify one or more
+   * attributes, corresponding to the vertex attributes used in the draw calls.
+   * Indexed primitives also define an indices property. Attributes and indices
+   * are defined as references to accessors containing corresponding data. Each
+   * primitive MAY also specify a material and a mode that corresponds to the
+   * GPU topology type (e.g., triangle set).
+   */
+  meshes?: Array<GLTFMesh>;
 
   /**
    * glTF assets MAY define nodes, that is, the objects comprising the scene to
@@ -318,7 +494,7 @@ export type GLTFAsset = {
    * The node named Car has four children. Each of those nodes could in turn
    * have its own children, creating a hierarchy of nodes.
    */
-  nodes?: Array<{}>;
+  nodes?: Array<GLTFNode>;
   /**
    * glTF 2.0 assets MAY contain zero or more scenes, the set of visual objects
    * to render. Scenes are defined in a scenes array.
