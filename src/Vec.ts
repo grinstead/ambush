55a193067e129e7eefd020dc2a03b36dbd0d13b1
Joe Grinstead <npm@mailforce.net>
2024-06-29 17:56:54 -0400

    Add more vector utilities

--- src/Vec.ts
+++ src/Vec.ts
@@ -30,6 +30,59 @@ export function vec(x: number, y: number, z: number = 0): Vec {
 export const VEC_ZERO = vec(0, 0, 0);
 
 /**
+ * Function to add two vectors
+ * @param a - First vector
+ * @param b - Second vector
+ * @returns A new vector that is the result of adding the two input vectors
+ */
+export function addVec(a: Vec, b: Vec): Vec {
+  return vec(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+/**
+ * Function to subtract one vector from another
+ * @param a - First vector
+ * @param b - Second vector to be subtracted from the first vector
+ * @returns A new vector that is the result of subtracting the second vector from the first vector
+ */
+export function subtractVec(a: Vec, b: Vec): Vec {
+  return vec(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+/**
+ * Function to scale a vector by a given factor
+ * @param a - The vector to be scaled
+ * @param scale - The factor by which to scale the vector
+ * @returns A new vector that is the result of scaling the input vector by the given factor.
+ *          If the scale factor is 1, the original vector is returned.
+ *          If the scale factor is 0, {@link VEC_ZERO} is returned.
+ */
+export function scale(a: Vec, scale: number): Vec {
+  return scale
+    ? scale === 1
+      ? a
+      : vec(a.x * scale, a.y * scale, a.z * scale)
+    : VEC_ZERO;
+}
+
+/**
+ * Function to scale the coordinates of a vector by given factors for each axis
+ * @param a - The vector to be scaled
+ * @param scaleX - The factor by which to scale the x-coordinate of the vector
+ * @param scaleY - The factor by which to scale the y-coordinate of the vector
+ * @param scaleZ - The factor by which to scale the z-coordinate of the vector (default is 1)
+ * @returns A new vector that is the result of scaling the input vector's coordinates by the given factors
+ */
+export function scaleCoords(
+  a: Vec,
+  scaleX: number,
+  scaleY: number,
+  scaleZ: number = 1
+): Vec {
+  return vec(a.x * scaleX, a.y * scaleY, a.z * scaleZ);
+}
+
+/**
  * Function to compute the dot product of two vectors
  * @param a - First vector
  * @param b - Second vector
