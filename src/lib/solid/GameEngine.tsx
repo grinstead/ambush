2e234166e269fce2cf7fe533ed5930b1b74e7ad3
Joe Grinstead <npm@mailforce.net>
2024-07-19 10:47:00 -0400

    Add useGameEngine convenience

--- src/lib/solid/GameEngine.tsx
+++ src/lib/solid/GameEngine.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "solid-js";
+import { createContext, useContext } from "solid-js";
 import { MouseAccessors } from "./mouse.tsx";
 import { Dimensions } from "./dims.tsx";
 
@@ -6,4 +6,8 @@ export const GameEngineContext = createContext<GameEngine>();
 
 export class GameEngine {
   constructor(readonly mouse: MouseAccessors, readonly area: Dimensions) {}
+}
+
+export function useGameEngine() {
+  return useContext(GameEngineContext)!;
 }
